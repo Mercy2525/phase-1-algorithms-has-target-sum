137805356c7a8a3d36565a1c9acbbc181f0973c5
ASUS <you@example.com>
2023-07-29 12:43:48 +0300

    Target

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  for(let i=0; i<array.length; i++){
+    let complement=target-array[i];
+    for(let j=1+i; j<=array.length; j++){
+      if (array[j]===complement) return true;
+      }
+    }
+    return false
+  } 
+
 
 /* 
   Write the Big O time complexity of your function here
+  the outer loop, loops through n times 0(n),
+  the inner loop,loops through the inner and outer loop n times
+  Big on being 0(n*n)
+  =O(n^2) 
 */
 
 /* 
   Add your pseudocode here
+  Loop through the array,
+  pick the first element, and find a complement=target-array[i]
+  loop through the remaining numbers from index 2, and they should a number matching the complement
+  if the second loop=compliment,  return true
+  if now match is found, return false
 */
 
 /*
   Add written explanation of your solution here
+  e.g if numbers = [1,2,4,5,6] and target is 9, the function should return true
+  since two of the numbers in the loop add up to 9
+  for numbers=[1,2,3,6,8], target is 20, the function should return false
+  since no pair in the array can give a sum of 20
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -32,3 +53,5 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+
